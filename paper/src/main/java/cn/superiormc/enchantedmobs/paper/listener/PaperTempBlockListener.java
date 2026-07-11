a4d366330cf1969e6da3d74c7ba3c3dfb27e120d
PQguanfang <pqguanfang@foxmail.com>
2026-07-11 22:49:34 +0800

    [1.2.7] Improve temp block codes.

--- paper/src/main/java/cn/superiormc/enchantedmobs/paper/listener/PaperTempBlockListener.java
+++ paper/src/main/java/cn/superiormc/enchantedmobs/paper/listener/PaperTempBlockListener.java
@@ -4,17 +4,19 @@ import java.util.Iterator;
 
 import cn.superiormc.enchantedmobs.EnchantedMobs;
 import cn.superiormc.enchantedmobs.managers.TempBlockManager;
+import cn.superiormc.enchantedmobs.utils.SchedulerUtil;
 import com.destroystokyo.paper.event.block.BlockDestroyEvent;
 import io.papermc.paper.event.block.BlockBreakBlockEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.entity.*;
 
 public class PaperTempBlockListener extends TempBlockManager implements Listener {
@@ -24,7 +26,7 @@ public class PaperTempBlockListener extends TempBlockManager implements Listener
         Bukkit.getPluginManager().registerEvents(this, EnchantedMobs.instance);
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent event) {
         Location location = event.getBlock().getLocation();
 
@@ -35,48 +37,78 @@ public class PaperTempBlockListener extends TempBlockManager implements Listener
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockExplode(BlockExplodeEvent event) {
         Iterator<Block> iterator = event.blockList().iterator();
         while (iterator.hasNext()) {
             Block exploded = iterator.next();
             if (isTempBlock(exploded.getLocation())) {
                 iterator.remove();
-                exploded.setType(Material.AIR);
                 removeTempBlock(exploded.getLocation());
             }
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onEntityExplode(EntityExplodeEvent event) {
         Iterator<Block> iterator = event.blockList().iterator();
         while (iterator.hasNext()) {
             Block exploded = iterator.next();
             if (isTempBlock(exploded.getLocation())) {
                 iterator.remove();
-                exploded.setType(Material.AIR);
                 removeTempBlock(exploded.getLocation());
             }
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockBreakBlock(BlockBreakBlockEvent event) {
         Block block = event.getBlock();
         if (isTempBlock(block.getLocation())) {
             event.getDrops().clear();
-            removeTempBlock(block.getLocation());
+            SchedulerUtil.runTaskLater(block.getLocation(), () -> removeTempBlock(block.getLocation()), 1L);
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockDestroy(BlockDestroyEvent event) {
         Block block = event.getBlock();
         if (isTempBlock(block.getLocation())) {
             event.setWillDrop(false);
             removeTempBlock(block.getLocation());
         }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPistonExtend(BlockPistonExtendEvent event) {
+        if (containsTempBlock(event.getBlocks(), event.getDirection())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPistonRetract(BlockPistonRetractEvent event) {
+        if (containsTempBlock(event.getBlocks(), event.getDirection())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        Location location = event.getBlock().getLocation();
+        if (isTempBlock(location)) {
+            event.setCancelled(true);
+            removeTempBlock(location);
+        }
+    }
+
+    private boolean containsTempBlock(Iterable<Block> blocks, org.bukkit.block.BlockFace direction) {
+        for (Block block : blocks) {
+            if (isTempBlock(block.getLocation()) || isTempBlock(block.getRelative(direction).getLocation())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
--- spigot/src/main/java/cn/superiormc/enchantedmobs/spigot/listener/SpigotTempBlockListener.java
+++ spigot/src/main/java/cn/superiormc/enchantedmobs/spigot/listener/SpigotTempBlockListener.java
@@ -4,13 +4,16 @@ import cn.superiormc.enchantedmobs.EnchantedMobs;
 import cn.superiormc.enchantedmobs.managers.TempBlockManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 import java.util.Iterator;
@@ -22,7 +25,7 @@ public class SpigotTempBlockListener extends TempBlockManager implements Listene
         Bukkit.getPluginManager().registerEvents(this, EnchantedMobs.instance);
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent event) {
         Location location = event.getBlock().getLocation();
 
@@ -33,30 +36,60 @@ public class SpigotTempBlockListener extends TempBlockManager implements Listene
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockExplode(BlockExplodeEvent event) {
         Iterator<Block> iterator = event.blockList().iterator();
         while (iterator.hasNext()) {
             Block exploded = iterator.next();
             if (isTempBlock(exploded.getLocation())) {
                 iterator.remove();
-                exploded.setType(Material.AIR);
                 removeTempBlock(exploded.getLocation());
             }
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onEntityExplode(EntityExplodeEvent event) {
         Iterator<Block> iterator = event.blockList().iterator();
         while (iterator.hasNext()) {
             Block exploded = iterator.next();
             if (isTempBlock(exploded.getLocation())) {
                 iterator.remove();
-                exploded.setType(Material.AIR);
                 removeTempBlock(exploded.getLocation());
             }
         }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPistonExtend(BlockPistonExtendEvent event) {
+        if (containsTempBlock(event.getBlocks(), event.getDirection())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPistonRetract(BlockPistonRetractEvent event) {
+        if (containsTempBlock(event.getBlocks(), event.getDirection())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        Location location = event.getBlock().getLocation();
+        if (isTempBlock(location)) {
+            event.setCancelled(true);
+            removeTempBlock(location);
+        }
+    }
+
+    private boolean containsTempBlock(Iterable<Block> blocks, BlockFace direction) {
+        for (Block block : blocks) {
+            if (isTempBlock(block.getLocation()) || isTempBlock(block.getRelative(direction).getLocation())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
